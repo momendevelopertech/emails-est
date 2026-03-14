b128c6363a0681a26df76d0cb3fb823498ae4a72
Galaxy Spectrum International <136253041+gsiom@users.noreply.github.com>
2026-03-14 15:25:14 +0200

    fix: exclude app-build-manifest from pwa precache

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,7 @@ const withPWA = require('next-pwa')({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
+    buildExcludes: [/app-build-manifest\.json$/],
 });
 
 const createNextIntlPlugin = require('next-intl/plugin');
