bbaed053382bf3700a94b1dd1ef1bb402153ce0f
momendevelopertech <momen.developer.tech@gmail.com>
2026-04-20 10:04:23 +0200

    Fix public confirm route and add responded recipients stat

---
+++ apps/web/src/app/[locale]/messaging/confirm/page.tsx
@@ -0,0 +1,5 @@
+import PublicRecipientResponseClient from '@/components/messaging/PublicRecipientResponseClient';
+
+export default function PublicMessagingConfirmPage() {
+    return <PublicRecipientResponseClient />;
+}
